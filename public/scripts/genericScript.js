7b876475f8e650326f6d960bcfb6f0a7c5de1081
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-07 10:05:19 -0600

    sorted by event name on click

--- public/scripts/genericScript.js
+++ public/scripts/genericScript.js
@@ -1,5 +1,7 @@
 console.log( 'genero sourced' );
 
+var eventsArray = [];
+
 $( document ).ready( function(){
   console.log( 'JQ' );
 
@@ -37,7 +39,9 @@ $( document ).ready( function(){
         var newEvent = events[events.length-1];
         console.log('parsed response', newEvent);
         console.log( 'back from post call:', response );
-
+        //push each new event into events array
+        eventsArray.push(newEvent);
+        //append events to DOM
         var eventsOutput = $('#eventsOutput');
         eventsOutput.addClass('row');
         eventsOutput.addClass('text-center');
@@ -72,5 +76,29 @@ $( document ).ready( function(){
     postData(eventName, athleteName, award);
     $('input').val('');
   }); // end testGetButton
+  $('#sortButton').on('click', function() {
+    console.log('sort button clicked');
+    eventsArray.sort(function(a, b){
+      return a.eventName > b.eventName;
+    }); // end sort eventsArray
+    console.log('after sorting',eventsArray);
+
+    $('#eventsOutput').html('');
+    for (var i = 0; i < eventsArray.length; i++) {
+      var eventsOutput = $('#eventsOutput');
+      eventsOutput.addClass('row');
+      eventsOutput.addClass('text-center');
+
+      eventsOutput.append('<div class="col-sm-4"></div>');
+      var $el = eventsOutput.children().last();
+      $el.append('<div class="event"></div>');
+      $eventDiv = $el.find('.event');
+
+      $eventDiv.append('<p>Event Name: '+eventsArray[i].eventName+'</p>');
+      $eventDiv.append('<p>Athlete Name: '+eventsArray[i].athleteName+'</p>');
+      $eventDiv.append('<p>Award: '+eventsArray[i].award+'</p>');
+
+    }
+  }); // end sortButton
 
 }); //end doc ready
